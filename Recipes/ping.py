2c2de6fd6c78e98f27c4bef02103d6c392ea8304
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-04-13 13:37:26 +0100

    Create ping.py

---
+++ Recipes/ping.py
@@ -0,0 +1,25 @@
+## Caclulate the ping of your app server
+## Not accurate
+
+from IPC import AppClient
+
+client = AppClient('localhost', 5000, 'mysecretkey')
+
+@client.on_call(event_name='ping')
+def get_ping():
+    return 'Called!'
+    
+## Web
+from IPC import WebClient
+from time import perf_counter
+from flask import Flask
+
+app = Flask(__name__)
+client = WebClient('localhost', 5000, 'mysecretkey')
+
+@app.route('/')
+def index():
+    start = perf_counter()
+    client.get('ping')
+    end = perf_counter()
+    return 'My Application servers ping is currently - {}ms'.format(round(end - start))
