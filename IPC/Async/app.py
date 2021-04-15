3a3b3cb141b6b1d3bb931ef5c979c35cf135545d
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-04-15 20:42:07 +0100

    Update app.py

--- IPC/Async/app.py
+++ IPC/Async/app.py
@@ -65,58 +65,8 @@ class AsyncAppClient:
 
         self.__calls.update({event_name: [func, methods]})
 
-    def client_connection_callback(self, cli_reader, cli_writer):
-        client_id = cli_writer.get_extra_info("peername")
-        print(client_id)
-
-        def client_cleanup(future):
-            try:
-                future.result()
-            except Exception:
-                pass
-        
-            ## del self.clients[client_id]
-        
-        task = asyncio.ensure_future(self.client_task(cli_reader, cli_writer))
-        task.add_done_callback(client_cleanup)
-        
-        ## self.clients[client_id] = task
-    
-    async def client_task(self, reader, writer):
-        while True:
-            data = await reader.read(1024)
-            
-            if not data:
-                continue
-
-            data = data.decode()
-            parsed_json = json.loads(data)
-            
-            headers = parsed_json.get("headers")
-            
-            if not headers or not headers.get("Authentication"):
-                response = {"error": "No authentication provided.", "status": 403}
-            
-            token = headers.get("Authentication")
-            
-            if token != self.secret_key:
-                response = {"error": "Invalid authorization token provided.", "status": 403}
-            else:
-                endpoint = parsed_json.get("endpoint")
-                
-                if not endpoint or not self.endpoints.get(endpoint):
-                    response = {"error": 'No endpoint matching {} was found.'.format(endpoint), "status": 404}
-                else:
-                    server_response = IpcServerResponse(parsed_json)
-                    response = await self.endpoints[endpoint](server_response)
-            
-            writer.write(json.dumps(response).encode("utf-8"))
-            await writer.drain()
-            
-            break
-
     def start(self):
-        server = start_server(client_connection_callback, self.host, self.port, loop=self.loop)
+        server = start_server(handle_connection, self.host, self.port, loop=self.loop)
         self.loop.run_until_complete(server)
 
         
